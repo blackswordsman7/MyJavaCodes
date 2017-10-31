d33ad371b711fe001bea839b72808ea34c0a3cd0
thesachinmittal <sachin.narenmittal@gmail.com>
2017-10-31 12:29:16 +0530

    Create Quadratic.java

---
+++ Quadratic.java
@@ -0,0 +1,23 @@
+//USE OF ARGS AND MATH FUNCTION IN JAVA   
+
+import java.lang.Math;
+
+public class Quadratic { 
+
+    public static void main(String[] args) { 
+    	//Use of args
+        double b = Double.parseDouble(args[0]);
+        double c = Double.parseDouble(args[1]);
+
+        double discriminant = b*b - 4.0*c;
+        //Use of function sqrt
+        double sqroot =  Math.sqrt(discriminant);
+
+        double root1 = (-b + sqroot) / 2.0;
+        double root2 = (-b - sqroot) / 2.0;
+
+        System.out.println(root1);
+        System.out.println(root2);
+    }
+}
+
